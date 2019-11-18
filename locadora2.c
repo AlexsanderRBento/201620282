60a0131da7cd2322d70b7b5175de90cf200370ad
AlexsanderRBento <50189771+AlexsanderRBento@users.noreply.github.com>
2019-11-18 03:48:55 -0300

    Update locadora2.c

--- locadora2.c
+++ locadora2.c
@@ -6,8 +6,10 @@
 
 
 
+
 int main() {
 int escolha;
+
 
 typedef struct filme
 {
@@ -49,39 +51,88 @@ struct cliente clientes;
 {
 	case 1:
 			system ("clear");
-	FILE *fp;
-	fp = fopen("movies.txt","ab");
+	FILE *f;
+	f = fopen("movies.txt","a");
 	printf("digite nome do filme\n");
 	setbuf(stdin, NULL);
 	fgets(filmes.nome,1000,stdin);
 	printf("digite o ID do filme\n");
 	setbuf(stdin, NULL);
 	fgets(filmes.id,1000,stdin);
-	fwrite(&filmes, sizeof(filmes),1000,fp);
-	fclose(fp);
+	fwrite(&filmes, sizeof(filmes),1000,f);
+	fclose(f);
 
 	break;
 
 
 	case 2:
 			system ("clear");
-	printf("digite nome do cliente\n");
-		FILE *cp;
-	cp = fopen("client.txt","ab");
+
+		FILE *c;
+	c = fopen("client.txt","a");
 	printf("digite nome do cliente\n");
 	setbuf(stdin, NULL);
 	fgets(clientes.nome,1000,stdin);
 	printf("digite o ID do cliente\n");
 	setbuf(stdin, NULL);
 	fgets(clientes.id,1000,stdin);
-	fwrite(&clientes, sizeof(clientes),1000,cp);
-	fclose(cp);
+	fwrite(&clientes, sizeof(clientes),1000,c);
+	fclose(c);
 	
 	break;
 	
 	case 3:
-			system ("clear");
+
+
+
+	FILE *fl = fopen("movies.txt", "rb");
+ 
+
+	if (fl == NULL)
+	{
+		printf("\nFalha ao abrir arquivo(s) ou ");
+		printf("Nenhum filme encontrado.\n");
+		printf("\nPressione <Enter> para continuar...");
+		scanf("%*c");
+ 
+		
+		fseek(stdin, 0, SEEK_END);
+		return;
+	}
+
+	int encontrou_filmes = 0;
+	filme filmes;
+ 
+	printf("\nLista\n");
+
+	while(1)
+	{
+
+		size_t result = fread(&filmes, sizeof(filme), 1, fl);
+ 
 	
+		if(result == 0)
+			break;
+ 
+		encontrou_filmes = 1;
+ 
+	
+		printf("\nID do filme: %d\n", filme.id);
+		printf("Nome do filme: %s\n", filme.nome);
+	}
+ 
+	if(encontrou_filmes == 0)
+		printf("\nNenhum filme cadastrado.\n");
+ 
+	fclose(fl);
+ 
+	printf("\nPressione <Enter> para continuar...");
+	scanf("%*c");
+ 
+
+	fseek(stdin, 0, SEEK_END);
+
+	    
 	break;
 
 	case 4:
@@ -94,30 +145,13 @@ struct cliente clientes;
 	
 	break;
 	
-	default:
-	printf("entrada invalida\n");
-	
-	
-}
-	
-return (0);
-
-}
-	
-	break;
-
-	case 4:
-			system ("clear");
-	
-	break;
-
-	case 5:
-			system ("clear");
-	
-	break;
+	case 0:
+	        exit(0);
 	
 	default:
 	printf("entrada invalida\n");
+	
+	
 	
 	
 }
